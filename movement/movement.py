ea6b90394df8d34604e76e11f73668ad395a12f5
Nikita Olechko <nikita.olechko@gmail.com>
2023-03-27 00:07:23 -0700

    Update movement checking with new board params

--- movement/movement.py
+++ movement/movement.py
@@ -50,11 +50,12 @@ def get_user_choice():
     return user_input
 
 
-def validate_move(board: dict, character: dict, direction: str) -> bool:
+def validate_move(board: dict, character: dict, direction: str, pokemon: dict) -> bool:
     """
     Verify that direction is a moveable direction within the board.
 
     A function that checks if direction will move the character to a valid position on the board
+    :param pokemon: a dictionary containing pokemon names as keys
     :param board: a dictionary containing tuples of length two containing positive integers
         as keys and strings as values
     :param character: a dictionary containing the character's current coordinates as a tuple of length two containing
@@ -64,6 +65,7 @@ def validate_move(board: dict, character: dict, direction: str) -> bool:
         two positive integers and their corresponding values as strings
     :precondition: character must be a dictionary containing the key values "X-coordinate", "X-coordinate", "Current HP"
     :precondition: direction must be a string from 1-4 or "up", "down", "left", or "right"
+    :precondition: pokemon must be a dictionary containing pokemon names as keys
     :postcondition: verifies if direction will move the user to a valid location on the board
     :return: True if direction moves character to a valid location on the board, else False
     >>> game_board = {(0,0): "Room", (0,1): "Room", (1,0): "Room", (1,1): "Room"}
@@ -78,13 +80,24 @@ def validate_move(board: dict, character: dict, direction: str) -> bool:
     That is not a valid move
     False
     """
+    if not direction_in_board(board, character, direction):
+        return False
+    if ocean_in_way(board, character, direction):
+        if can_cross_ocean(character, pokemon):
+            return True
+        else:
+            return False
+    return True
+
+
+def direction_in_board(board, character, direction):
     direction_dict = {"up": -1, "down": 1, "left": -1, "right": 1, "1": -1, "2": 1, "3": -1, "4": 1}
     current_position = (character['X-coordinate'], character['Y-coordinate'])
     if current_position not in board:
         return False
     if direction == "up" or direction == "down" or direction == "1" or direction == "2":
         new_y = character['Y-coordinate'] + direction_dict[direction]
-        if (character['Y-coordinate'], new_y) not in board:
+        if (character['X-coordinate'], new_y) not in board:
             print("That is not a valid move")
             return False
     else:
@@ -93,6 +106,47 @@ def validate_move(board: dict, character: dict, direction: str) -> bool:
             print("That is not a valid move")
             return False
     return True
+
+
+def check_for_foes():
+    """
+    Return True 1/3 of the time.
+
+    A function that ensures a True is returned 1/3 of the time.
+    :postcondition: returns True 1/3 of the time
+    :return: True 1/3 of the time, False 2/3 of the time
+    """
+    if random.randint(1, 3) == 3:
+        return True
+    return False
+
+
+def ocean_in_way(board: dict, character: dict, direction: str) -> bool:
+    direction_dict = {"up": -1, "down": 1, "left": -1, "right": 1, "1": -1, "2": 1, "3": -1, "4": 1}
+    if direction == "up" or direction == "down" or direction == "1" or direction == "2":
+        new_y = character['Y-coordinate'] + direction_dict[direction]
+        if board[(character['X-coordinate'], new_y)] == "Ocean":
+            return True
+    else:
+        new_x = character['X-coordinate'] + direction_dict[direction]
+        if board[(new_x, character['Y-coordinate'])] == "Ocean":
+            return True
+    return False
+
+
+def can_cross_ocean(character: dict, pokemon: dict) -> bool:
+    if character["Boat"] or has_rideable_pokemon(pokemon):
+        return True
+    print("Hmmm...you need some way to cross the water...")
+    return False
+
+
+def has_rideable_pokemon(pokemon):
+    list_of_rideable_pokemon = ["gyarados", "kyogre"]
+    for ride in list_of_rideable_pokemon:
+        if ride in pokemon.values():
+            return True
+    return False
 
 
 def move_character(character: dict, direction: str):
@@ -122,33 +176,3 @@ def move_character(character: dict, direction: str):
         character['Y-coordinate'] += direction_dict[direction]
     else:
         character['X-coordinate'] += direction_dict[direction]
-
-
-def check_for_foes():
-    """
-    Return True 1/3 of the time.
-
-    A function that ensures a True is returned 1/3 of the time.
-    :postcondition: returns True 1/3 of the time
-    :return: True 1/3 of the time, False 2/3 of the time
-    """
-    if random.randint(1, 3) == 3:
-        return True
-    return False
-
-
-def check_for_ocean(board: dict, character: dict, direction: str) -> bool:
-    current_position = (character['X-coordinate'], character['Y-coordinate'])
-    if current_position not in board:
-        return False
-    if direction == "up" or direction == "down" or direction == "1" or direction == "2":
-        new_y = character['Y-coordinate'] + direction_dict[direction]
-        if (character['Y-coordinate'], new_y) not in board:
-            print("That is not a valid move")
-            return False
-    else:
-        new_x = character['X-coordinate'] + direction_dict[direction]
-        if (new_x, character['X-coordinate']) not in board:
-            print("That is not a valid move")
-            return False
-    return True
