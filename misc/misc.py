6e01d8d9976f5fe53d10ef1b3e9dfb1a71f1c8fe
Nikita Olechko <nikita.olechko@gmail.com>
2023-03-30 14:01:03 -0700

    Create random_multiplier()

---
+++ misc/misc.py
@@ -0,0 +1,7 @@
+import random
+
+
+def random_multiplier(number_to_multiply):
+    number = random.choice([0.90, 0.92, 0.94, 0.96, 0.98, 1.0, 1.02, 1.04, 1.06, 1.08, 1.1])
+    rounded_number = int(round(number*number_to_multiply))
+    return rounded_number
