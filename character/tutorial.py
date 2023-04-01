023f11a778ebb078eca04893b8eba2372fbf2901
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-01 00:08:45 -0700

    Initialize tutorial

---
+++ character/tutorial.py
@@ -0,0 +1,12 @@
+Iimport time
+
+
+def tutorial():
+    print(".", end='')
+    time.sleep(0.5)
+    print(".", end='')
+    time.sleep(0.5)
+    print(".", end='')
+    time.sleep(0.5)
+    print("?? What's this??", end='')
+    print("Well hello, look who's finally awoken?")
