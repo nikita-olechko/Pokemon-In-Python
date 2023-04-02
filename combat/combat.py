ded5b6947266931af72dc2c6c12f8a41a646ff3e
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-02 16:17:11 -0700

    Implement pokemon swapping system

--- combat/combat.py
+++ combat/combat.py
@@ -1,8 +1,9 @@
 import random
 
 from character.leveling import level_up
-from utilities.utilities import randomize_within_10_percent
-from pokemon.finding_pokemon import get_a_random_pokemon_by_location, get_pokemon_dict
+from character.tutorial import yes_or_no_input
+from utilities.utilities import randomize_within_10_percent, print_rolling_dialogue
+from pokemon.finding_pokemon import get_a_pokemon_by_location, get_pokemon_dict
 from pokemon.moves import get_moves
 
 from playsound import playsound
@@ -38,9 +39,6 @@ def choose_a_pokemon(pokemon_inventory):
         elif pokemon_inventory[chosen_pokemon]["Current HP"] <= 0:
             print(f"{chosen_pokemon.title()} is unconscious")
             continue
-        # elif chosen_pokemon.isdigit():
-
-        #     return poke_dict[chosen_pokemon]
         else:
             return chosen_pokemon
 
@@ -100,6 +98,19 @@ def has_conscious_pokemon(pokemon_inventory):
     return False
 
 
+def swap_pokemon(pokemon_inventory, enemy_name, board, character):
+    print_rolling_dialogue(f"\nYou can't carry anymore Pokemon! Would you like to swap out a Pokemon for {enemy_name}?")
+    chosen_pokemon = choose_a_pokemon(pokemon_inventory)
+    print_rolling_dialogue(f"\nAre you sure you want to swap out {chosen_pokemon} for {enemy_name}?")
+    if yes_or_no_input():
+        del pokemon_inventory[chosen_pokemon]
+        pokemon_inventory[enemy_name] = get_a_pokemon_by_location(board, character,
+                                                                  enemy_name)[enemy_name]
+        pokemon_inventory[enemy_name]['Current HP'] = 0
+
+
+
+
 def victory_sequence(pokemon_inventory, enemy_name, character, board):
     exp_gain = randomize_within_10_percent(50)
     gold_gain = randomize_within_10_percent(50)
@@ -113,21 +124,21 @@ def victory_sequence(pokemon_inventory, enemy_name, character, board):
             return
         capture = input(f"Capture {enemy_name.title()}?\n\t1: Yes, 2: No\t\n").lower()
         if capture in ['yes', 'no', '1', '2']:
-            if capture in ['yes', '1'] and character["Pokeballs"] > 0:
-                pokemon_inventory[enemy_name] = get_a_random_pokemon_by_location(board, character,
-                                                                                 enemy_name)[enemy_name]
+            if capture in ['yes', '1'] and character["Pokeballs"] > 0 and len(pokemon_inventory) < 6:
+                pokemon_inventory[enemy_name] = get_a_pokemon_by_location(board, character,
+                                                                          enemy_name)[enemy_name]
                 pokemon_inventory[enemy_name]['Current HP'] = 0
                 character["Pokeballs"] -= 1
                 print(f"You have captured {enemy_name.title()}! To revive {enemy_name.title()}, "
                       f"return to the hospital.")
-                level_up(character, pokemon_inventory)
                 return
             elif capture in ['yes', '1'] and character["Pokeballs"] == 0:
                 print("You do not have any Pokéballs left!")
-                level_up(character, pokemon_inventory)
+                return
+            elif capture in ['yes', '1'] and len(pokemon_inventory) >= 6:
+                swap_pokemon(pokemon_inventory, enemy_name, board, character)
                 return
             else:
-                level_up(character, pokemon_inventory)
                 return
 
 
@@ -178,13 +189,14 @@ def combat_loop(character, board, pokemon_inventory, enemy_name, enemy_stats, cu
         return False
     if victory:
         victory_sequence(pokemon_inventory, enemy_name, character, board)
+        level_up(character, pokemon_inventory)
         return True
 
 
-def initialize_combat(character, board, pokemon_inventory, enemy_name=None):
+def get_combat_details(character, board, pokemon_inventory, enemy_name=None):
     initialize_arceus_music(enemy_name)
     if enemy_name is None:
-        enemy_stats = get_a_random_pokemon_by_location(board, character)
+        enemy_stats = get_a_pokemon_by_location(board, character)
         enemy_name = list(enemy_stats.keys())[0]
         print(f"\n\tYou encounter a wild {enemy_name.title()}!")
     else:
