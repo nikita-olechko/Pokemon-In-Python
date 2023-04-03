34be91bc2d59d5f7a67cf8fed7445a856e7a8642
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-03 16:47:44 -0700

    Refactor moves files and functions

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -2,8 +2,6 @@ import json
 import random
 import time
 
-from pokemon.moves import get_moves
-
 
 def randomize_within_10_percent(number_to_multiply):
     number = random.choice([0.90, 0.92, 0.94, 0.96, 0.98, 1.0, 1.02, 1.04, 1.06, 1.08, 1.1])
