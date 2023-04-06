4b2ebb5b7b10128f65d5f1d4279af3ea8cde3269
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-05 23:01:25 -0700

    Create test_move_character.py

--- old_tests/test_move_character.py
+++ old_tests/test_move_character.py
@@ -12,24 +12,24 @@ from game import move_character
 class TestMoveCharacter(TestCase):
     def test_move_down(self):
         player = {"X-coordinate": 0, "Y-coordinate": 0}
-        direct = "2"
+        direct = "s"
         move_character(player, direct)
         self.assertEqual(player, {"X-coordinate": 0, "Y-coordinate": 1})
 
     def test_move_up(self):
         player = {"X-coordinate": 0, "Y-coordinate": 1}
-        direct = "1"
+        direct = "w"
         move_character(player, direct)
         self.assertEqual(player, {"X-coordinate": 0, "Y-coordinate": 0})
 
     def test_move_left(self):
         player = {"X-coordinate": 1, "Y-coordinate": 0}
-        direct = "3"
+        direct = "a"
         move_character(player, direct)
         self.assertEqual(player, {"X-coordinate": 0, "Y-coordinate": 0})
 
     def test_move_right(self):
         player = {"X-coordinate": 0, "Y-coordinate": 0}
-        direct = "4"
+        direct = "d"
         move_character(player, direct)
         self.assertEqual(player, {"X-coordinate": 1, "Y-coordinate": 0})
