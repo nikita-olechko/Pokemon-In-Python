0fc779d63fb7aa658083eea881343b67451a337b
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-05 17:49:41 -0700

    Update docstrings

--- pokemon/moves.py
+++ pokemon/moves.py
@@ -1,26 +1,11 @@
-import json
-
 from pokemon.finding_pokemon import read_json
 
 
 def get_moves() -> dict:
+    """
+    Retrieves all moves from a json file.
+    @postcondition: retrieves dictionary of moves
+    @return: dictionary of moves with move names as keys and values as stats
+    """
     all_moves = read_json("json_data/moves.json")
     return all_moves
-
-
-def read_and_write_json_moves(file):
-    with open(file) as json_file:
-        move_dict = json.load(json_file)
-    all_moves = get_moves()
-    for move in all_moves:
-        move_dict[move] = all_moves[move]
-    with open(file, "w") as json_file:
-        json.dump(move_dict, json_file)
-
-
-def standardize_location(func):
-    func_dict = func
-    new_dict = func_dict
-    for pokemon in func_dict:
-        new_dict[pokemon]["Location"] = "Mine"
-    print(func_dict)
