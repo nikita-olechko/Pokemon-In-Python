caeb5bf221afd2519030461445ab7d5f29f3af53
Nikita Olechko <nikita.olechko@gmail.com>
2023-04-07 10:36:34 -0700

    Create saving system with docstrings

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -8,10 +8,10 @@ def randomize_within_10_percent(number_to_randomize):
     Randomizes a number in a 10% margin.
 
     A function that randomizes a number +/- 10%
-    @param number_to_randomize: a number to randomize
-    @precondition: number_to_randomize must be a float or an int
-    @postcondition: randomizes number_to_randomize +/- 10%
-    @return: number_to_randomize randomized +/- 10% as an integer
+    :param number_to_randomize: a number to randomize
+    :precondition: number_to_randomize must be a float or an int
+    :postcondition: randomizes number_to_randomize +/- 10%
+    :return: number_to_randomize randomized +/- 10% as an integer
     """
     number = random.choice([0.90, 0.92, 0.94, 0.96, 0.98, 1.0, 1.02, 1.04, 1.06, 1.08, 1.1])
     rounded_number = int(round(number * number_to_randomize))
@@ -21,13 +21,13 @@ def randomize_within_10_percent(number_to_randomize):
 def print_rolling_dialogue(string, delay=0.03, new_line=True):
     """
     Prints a string with a specified delay per character, with the default delay at 0.03 seconds.
-    @param string: a string
-    @param delay: an optional number representing the delay (in seconds) between each character in string
-    @param new_line: an optional boolean indicating whether a new line should be printed at the end of string
-    @precondition: string must be a string
-    @precondition: delay must be a number
-    @precondition: new_line must be a boolean operator
-    @postcondition: prints string character by character with the specified delay
+    :param string: a string
+    :param delay: an optional number representing the delay (in seconds) between each character in string
+    :param new_line: an optional boolean indicating whether a new line should be printed at the end of string
+    :precondition: string must be a string
+    :precondition: delay must be a number
+    :precondition: new_line must be a boolean operator
+    :postcondition: prints string character by character with the specified delay
     """
     for char in string:
         time.sleep(delay)
@@ -63,8 +63,8 @@ def all_prefixes(string: str) -> list:
 def yes_or_no_input():
     """
     Prompts the user yes or no (y/n).
-    @postcondition: gets user input on something
-    @return: True if yes (y), False if no (n)
+    :postcondition: gets user input on something
+    :return: True if yes (y), False if no (n)
     """
     while True:
         do_tutorial = input("(y/n): ").lower()
@@ -80,11 +80,11 @@ def yes_or_no_input():
 def read_json(file):
     """
     Returns data from a json file.
-    @param file: the name of a json file
-    @precondition: file must be the name of a json pile
-    @precondition: file name must start from the root of the working directory
-    @return: data inside the json file as a dictionary
+    :param file: the name of a json file
+    :precondition: file must be the name of a json pile
+    :precondition: file name must start from the root of the working directory
+    :return: data inside the json file as a dictionary
     """
     with open(file) as json_file:
-        poke_dict = json.load(json_file)
-    return poke_dict
+        file_dict = json.load(json_file)
+    return file_dict
